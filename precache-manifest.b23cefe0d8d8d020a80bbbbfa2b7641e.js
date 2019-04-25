3e442dd0d07264329f4ec95dc1f822673d343c2a
janakhpon <jnovaxer@gmail.com>
2019-04-26 04:39:15 +0630

    Updates

---
+++ precache-manifest.b23cefe0d8d8d020a80bbbbfa2b7641e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c087678cdd81b51ae367",
+    "url": "/movieer/static/js/runtime~main.c087678c.js"
+  },
+  {
+    "revision": "a5e6e285c6f7a110bf3c",
+    "url": "/movieer/static/js/main.a5e6e285.chunk.js"
+  },
+  {
+    "revision": "610148490e58997f4cee",
+    "url": "/movieer/static/js/2.61014849.chunk.js"
+  },
+  {
+    "revision": "a5e6e285c6f7a110bf3c",
+    "url": "/movieer/static/css/main.48be8b22.chunk.css"
+  },
+  {
+    "revision": "528b54fa64787a93c1362aec04afcd7a",
+    "url": "/movieer/index.html"
+  }
+];
